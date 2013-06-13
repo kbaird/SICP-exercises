7c5635ae2b160f2478e0cb64a6065a49df1f8a96
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-06-13 11:20:30 -0700

    Add Segment#midpoint to ex2_02.rb

--- Ch02/ex2_02.rb
+++ Ch02/ex2_02.rb
@@ -20,4 +20,20 @@ class Segment
   def initialize(start_pt, end_pt)
     @start_pt, @end_pt = start_pt, end_pt
   end
+  def midpoint
+    mid_x = average(start_pt.x, end_pt.x)
+    mid_y = average(start_pt.y, end_pt.y)
+    Point.new(mid_x, mid_y)
+  end
+  private
+  def average(x1, x2)
+    (x1 + x2) / 2.0
+  end
+end
+
+if (__FILE__ == $0)
+  pt1 = Point.new(0,0)
+  pt2 = Point.new(3,4)
+  s1  = Segment.new(pt1,pt2)
+  puts s1.midpoint.inspect
 end
