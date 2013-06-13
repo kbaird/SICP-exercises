387be5b658f17366c8e05d00e70837e7731c0cce
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-06-13 11:07:32 -0700

    Add midpoint-segment to ex2_02.scm

--- Ch02/ex2_02.scm
+++ Ch02/ex2_02.scm
@@ -9,6 +9,11 @@
   (car seg))
 (define (end-segment seg)
   (cdr seg))
+(define (midpoint-segment seg)
+  (define (average x y) (/ (+ x y) 2))
+  (make-point
+    (average (x-point (start-segment seg)) (x-point (end-segment seg)))
+    (average (y-point (start-segment seg)) (y-point (end-segment seg)))))
 (define (make-point x y)
   (cons x y))
 (define (x-point pt)
