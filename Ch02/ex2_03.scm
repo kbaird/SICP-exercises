298c87a07389982a9a1b1aea293dfcc774b5eb3b
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-06-14 15:48:55 -0700

    Add ex2_03.scm

---
+++ Ch02/ex2_03.scm
@@ -0,0 +1,37 @@
+;; ex2_02.scm
+;; Kevin C. Baird
+;; SICP
+;; Exercise 2.3 - Represent rectangles on a plane
+
+(load "ex2_02.scm")
+
+(define (make-rectangle ul ur ll lr)
+  (list ul ur ll lr))
+
+(define (ul rect) (car    rect))
+(define (ur rect) (cadr   rect))
+(define (ll rect) (caddr  rect))
+(define (lr rect) (cadddr rect))
+
+(define (side-length pt1 pt2)
+  (define x-distance
+    (- (x-point pt1) (x-point pt2)))
+  (define y-distance
+    (- (y-point pt1) (y-point pt2)))
+  (abs (+ x-distance y-distance)))
+
+(define (area rect)
+  (* (side-length (ul rect) (ur rect))
+     (side-length (ur rect) (lr rect))))
+
+(define (perimeter rect)
+  (+ (side-length (ul rect) (ur rect))
+     (side-length (ur rect) (lr rect))
+     (side-length (lr rect) (ll rect))
+     (side-length (ll rect) (ul rect))))
+
+(define ul-pt (make-point 3 0))
+(define ur-pt (make-point 3 4))
+(define ll-pt (make-point 0 0))
+(define lr-pt (make-point 0 4))
+(define rect1 (make-rectangle ul-pt ur-pt ll-pt lr-pt))
