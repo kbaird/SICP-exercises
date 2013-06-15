143d76c9e1f1a0a4d9beeac0844a6cb88ce61d6c
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-06-14 18:44:12 -0700

    Apply abs to each of x-distance and y-distance, not just their sum, in ex2_03.scm

--- Ch02/ex2_03.scm
+++ Ch02/ex2_03.scm
@@ -15,10 +15,10 @@
 
 (define (side-length pt1 pt2)
   (define x-distance
-    (- (x-point pt1) (x-point pt2)))
+    (abs (- (x-point pt1) (x-point pt2))))
   (define y-distance
-    (- (y-point pt1) (y-point pt2)))
-  (abs (+ x-distance y-distance)))
+    (abs (- (y-point pt1) (y-point pt2))))
+  (+ x-distance y-distance))
 
 (define (area rect)
   (* (side-length (ul rect) (ur rect))
@@ -31,7 +31,7 @@
      (side-length (ll rect) (ul rect))))
 
 (define ul-pt (make-point 3 0))
-(define ur-pt (make-point 3 4))
+(define ur-pt (make-point 3 -4))
 (define ll-pt (make-point 0 0))
-(define lr-pt (make-point 0 4))
+(define lr-pt (make-point 0 -4))
 (define rect1 (make-rectangle ul-pt ur-pt ll-pt lr-pt))
