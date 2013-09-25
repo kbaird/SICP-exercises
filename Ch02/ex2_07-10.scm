d9765298b7f3bdc23f3853b8adb1e6736f8dca33
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-09-25 13:59:43 -0700

    Rename ex2_07-9.scm -> ex2_07-10.scm; add spans-zero?

--- Ch02/ex2_07-10.scm
+++ Ch02/ex2_07-10.scm
@@ -1,9 +1,10 @@
 ;; ex2_07-9.scm
 ;; Kevin C. Baird
 ;; SICP
-;; Exercise 2.7 - Interval Arithmetic
-;; Exercise 2.8 - add sub-interval
-;; Exercise 2.9 - add width
+;; Exercise 2.7  - Interval Arithmetic
+;; Exercise 2.8  - add sub-interval
+;; Exercise 2.9  - add width
+;; Exercise 2.10 - add spans-zero? error handling
 
 (define (add-interval x y)
   (make-interval (+ (lower-bound x) (lower-bound y))
@@ -18,9 +19,17 @@
                    (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
-  (mult-interval x
-                 (make-interval (/ 1.0 (upper-bound y))
-                                (/ 1.0 (lower-bound y)))))
+  (define (spans-zero? r)
+    (define x (lower-bound r))
+    (define y (upper-bound r))
+    (or
+      (and (>= x 0) (<= y 0))
+      (and (<= x 0) (>= y 0))))
+  (if (spans-zero? x y)
+    (raise "Can't divide when spanning zero.")
+    (mult-interval x
+                   (make-interval (/ 1.0 (upper-bound y))
+                                  (/ 1.0 (lower-bound y))))))
 
 (define (make-interval a b) (cons a b))
 (define upper-bound cdr)
