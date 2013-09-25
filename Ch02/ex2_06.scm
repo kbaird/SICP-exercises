1a583502f604fb3c30ce21e5c002978182e04345
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-09-25 12:01:19 -0700

    Expand ex2_06.scm; add plus, plus-alt, and church-encode

--- Ch02/ex2_06.scm
+++ Ch02/ex2_06.scm
@@ -8,12 +8,25 @@
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
-; define one
-(define one
-  (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x)))))
-
-; define two
-(define (two)
-  (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x)))) f) x)))))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define three (lambda (f) (lambda (x) (f (f (f x))))))
 
 ; define +
+(define (plus n m)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
+(define plus-alt (lambda (m) (lambda (n) ((m add1) n))))
+
+
+(define four (church-encode 4))
+
+(define (church-encode n)
+  (define (inner-encode f n)
+    (if (= n 0)
+      x
+      (f (inner-encode f (- n 1)))))
+  (lambda (f) (lambda (x) (inner-encode f n)))
+
+; cf. http://stackoverflow.com/questions/3912341/arithmetic-with-church-numerals
+; http://en.wikipedia.org/wiki/Church_encoding
