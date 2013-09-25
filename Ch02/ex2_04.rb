1dc7d3325ec6ae7ce2ef67e213b7654711cc588f
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-09-25 14:38:11 -0700

    Replace lambda with -> & .call with []

--- Ch02/ex2_04.rb
+++ Ch02/ex2_04.rb
@@ -7,20 +7,20 @@ SICP in Ruby
 Exercise 2.4 - cons, car, cdr as lamba expressions
 =end
 
-cons = lambda do |x,y|
-  lambda { |m| m.call(x,y) }
+cons = ->(x,y) do
+  ->(m) { m[x,y] }
 end
 
-car = lambda do |z|
-  z.call(lambda { |p,q| p })
+car = ->(z) do
+  z[->(p,q) { p }]
 end
 
-cdr = lambda do |z|
-  z.call(lambda { |p,q| q })
+cdr = ->(z) do
+  z[->(p,q) { q }]
 end
 
 if __FILE__ == $0
-  l = cons.call 1,2
-  puts car.call l
-  puts cdr.call l
+  l = cons[1,2]
+  puts car[l]
+  puts cdr[l]
 end
