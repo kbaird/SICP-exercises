e05d83ea0bc744f65a3af51402569793e66cfcaf
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-01-09 13:49:31 -0800

    Add Ex1_03.scala

---
+++ Ch01/Ex1_03.scala
@@ -0,0 +1,22 @@
+/**
+  * Ex1_03.scala
+  * Kevin C. Baird
+  * SICP in Scala
+  *
+  * Write a function that takes 3 numbers as arguments and returns
+  * the sum of the squares of the 2largest arguments.
+  */
+
+class Ex1_03(
+) {
+  def square(x: Int) = { x*x }
+
+  def sumSquares2Largest(a: Int, b: Int, c: Int) = {
+      var args         = Array(a, b, c)
+      var lowest       = args.min
+      var lowestSquare = square(lowest)
+      var squaresL     = args.map(square)
+      var sums         = squaresL.sum
+    sums - lowestSquare
+  }
+}
