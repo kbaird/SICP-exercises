d607ce432ba20a2fc68a181b4002d7f30d5237b3
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-01-10 09:44:51 -0800

    Add running notes to Ex1_03.scala

--- Ch01/Ex1_03.scala
+++ Ch01/Ex1_03.scala
@@ -4,11 +4,10 @@
   * SICP in Scala (http://www.scala-lang.org/documentation/)
   *
   * Write a function that takes 3 numbers as arguments and returns
-  * the sum of the squares of the 2largest arguments.
+  * the sum of the squares of the 2 largest arguments.
   */
 
-class Ex1_03(
-) {
+object Ex1_03 extends App {
   def square(x: Int) = { x*x }
 
   def sumSquares2Largest(a: Int, b: Int, c: Int) = {
@@ -20,3 +19,9 @@ class Ex1_03(
     sums - lowestSquare
   }
 }
+
+/**
+  * execute with
+  * scala -i Ex1_03.scala -e "println(Ex1_03.sumSquares2Largest(1, 3, -4))"
+  * until I figure out a better way
+  */
