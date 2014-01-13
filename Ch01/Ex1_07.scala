2e27788f5c7313994b2641d95b50cef56c29fd31
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-01-12 19:11:39 -0800

    Move several function definitions inside others

--- Ch01/Ex1_07.scala
+++ Ch01/Ex1_07.scala
@@ -8,26 +8,22 @@
   */
 
 object NewtonSquare extends App {
-
-  def sqrt(x: Double) = { sqrtIter(1.0, x) } /* TODO: figure out point-free */
-
-  def improve(guess: Double, x: Double) = {
-    (guess + (x / guess)) / 2.0
-  }
-
-  def goodEnough(guess: Double, x: Double) = {
-    (math.abs((square(guess)) - x)) < tolerance(guess)
-  }
-
-  def square(x: Double) = { x * x }
-
-  def tolerance(x: Double) = { x * 0.001 } /* TODO: figure out point-free */
-
-  def sqrtIter(guess: Double, x: Double): Double = {
-    goodEnough(guess, x) match {
-      case true  => guess
-      case false => sqrtIter(improve(guess, x), x)
+  def sqrt(x: Double) = {
+    def sqrtIter(guess: Double, x: Double): Double = {
+      def improve(guess: Double, x: Double) = {
+        (guess + (x / guess)) / 2.0
+      }
+      def goodEnough(guess: Double, x: Double) = {
+        def square(x: Double)    = { x * x }
+        def tolerance(x: Double) = { x * 0.001 } /* TODO: figure out point-free */
+        (math.abs((square(guess)) - x)) < tolerance(guess)
+      }
+      goodEnough(guess, x) match {
+        case true  => guess
+        case false => sqrtIter(improve(guess, x), x)
+      }
     }
+    sqrtIter(1.0, x) /* TODO: figure out point-free */
   }
 }
 
