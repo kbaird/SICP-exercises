78b8d35de429a61c4821eab6aa13fc1a079cddc4
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-02-24 08:26:52 -0800

    Switch from built-in Range to new Interval case calss to handle Double end pts

--- Ch02/Ex2_07-10.scala
+++ Ch02/Ex2_07-10.scala
@@ -10,50 +10,46 @@
   *   Exercise 2.10 - Add _spans_zero error handling
   */
 
-def addRanges(r1: Range, r2: Range) = {
-  val newHead = r1.head + r2.head
-  val newEnd  = r1.end  + r2.end
-  newHead to newEnd
-}
-
-/* Scala Ranges have Int members. TODO
-def divRanges(r1: Range, r2: Range) = {
-  val recipHead = (1.0 / r2.head): Double
-  val recipEnd  = (1.0 / r2.end): Double
-  val recip     = (recipHead to recipEnd): Range
-  multRanges(r1, recip)
-}
-*/
-
-def multRanges(r1: Range, r2: Range) = {
-  val p1 = r1.head * r2.head
-  val p2 = r1.head * r2.end
-  val p3 = r1.end  * r2.head
-  val p4 = r1.end  * r2.end
-  val newHead = List(p1, p2, p3, p4).min
-  val newEnd  = List(p1, p2, p3, p4).max
-  newHead to newEnd
-}
-
-def subRanges(r1: Range, r2: Range) = {
-  val newHead = r2.head - r1.head
-  val newEnd  = r2.end  - r1.end
-  newHead to newEnd
-}
-
-def rangeWidth(r: Range): Double = {
-  (r.end - r.head) / 2.0
+case class Interval(head: Double, end: Double) {
+  override def toString(): String = "Interval(" + head + ", " + end + ")";
+  def add(otherI: Interval): Interval = {
+    val newH = head + otherI.head
+    val newE = end  + otherI.end
+    Interval(newH, newE)
+  }
+  def div(otherI: Interval): Interval = {
+    val recipH = (1.0 / otherI.head): Double
+    val recipE = (1.0 / otherI.end): Double
+    val recip  = Interval(recipH, recipE)
+    mult(recip)
+  }
+  def mult(otherI: Interval): Interval = {
+    val p1 = head * otherI.head
+    val p2 = head * otherI.end
+    val p3 = end  * otherI.head
+    val p4 = end  * otherI.end
+    val newH = List(p1, p2, p3, p4).min
+    val newE = List(p1, p2, p3, p4).max
+    Interval(newH, newE)
+  }
+  def sub(otherI: Interval): Interval = {
+    val newH = head - otherI.head
+    val newE = end  - otherI.end
+    Interval(newH, newE)
+  }
+  def width: Double = { (end - head) / 2.0 }
 }
 
 def output() = {
-  val r1 = 2 to 9
-  val r2 = 3 to 5
-  (r1, r2, 
-   addRanges(r1, r2), 
-   multRanges(r1, r2), 
-   subRanges(r2, r1), 
-   /* divRanges(r1, r2), */
-   rangeWidth(r1))
+  val i1 = new Interval(2, 9)
+  val i2 = new Interval(3, 5)
+  (i1, i2,
+   i1.add(i2),
+   i1.sub(i2),
+   i1.mult(i2),
+   i1.div(i2),
+   i1.width
+  )
 }
 
 /**
