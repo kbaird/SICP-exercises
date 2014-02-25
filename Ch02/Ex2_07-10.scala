1d95da6632662600b6b70ac3acb0fc859478b355
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-02-25 09:15:12 -0800

    Add spansZero to Ex2_07-10.scala, not yet working

--- Ch02/Ex2_07-10.scala
+++ Ch02/Ex2_07-10.scala
@@ -10,6 +10,7 @@
   *   Exercise 2.10 - Add _spans_zero error handling
   */
 
+case class SpansZeroException(message: String) extends Exception(message)
 case class Interval(head: Double, end: Double) {
   override def toString(): String = "Interval(" + head + ", " + end + ")";
   def add(otherI: Interval): Interval = {
@@ -18,7 +19,11 @@ case class Interval(head: Double, end: Double) {
     Interval(newH, newE)
   }
   def div(otherI: Interval): Interval = {
-    /* TODO: add spans zero error handling */
+    val spansZero = 
+      (head >= 0 && end <= 0) ||
+      (head <= 0 && end >= 0)
+    if (spansZero)
+      throw SpansZeroException("")
     val recipH = (1.0 / otherI.head): Double
     val recipE = (1.0 / otherI.end): Double
     val recip  = Interval(recipH, recipE)
@@ -44,11 +49,13 @@ case class Interval(head: Double, end: Double) {
 def output() = {
   val i1 = new Interval(2, 9)
   val i2 = new Interval(3, 5)
+  val i3 = new Interval(-1, 5)
   (i1, i2,
    i1.add(i2),
    i1.sub(i2),
    i1.mult(i2),
    i1.div(i2),
+   i1.div(i3),
    i1.width
   )
 }
