e9d74f2d3e169a0f0bdb1c376c449e358a763612
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-03-26 10:40:51 -0700

    Add ex2_23.scm

---
+++ Ch02/ex2_23.scm
@@ -0,0 +1,14 @@
+;; ex2_23.scm
+;; Kevin C. Baird
+;; SICP
+;; Exercise 2.23 - for-each
+
+(define (for-each f items)
+ (cond ((null? items) #t)
+  (else (f (car items))
+   (for-each f (cdr items)))))
+
+(for-each (lambda (x)
+           (newline)
+           (display x))
+ (list 57 321 88))
