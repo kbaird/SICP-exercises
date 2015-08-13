235034f1ae0851d801db06d975d1463fdbfcc60c
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-13 17:07:46 -0400

    ex2_03.exs: Remove leading :ul, :ur, :ll, :lr symbols for Rectangle corners
    Made it compile

--- Ch02/ex2_03.exs
+++ Ch02/ex2_03.exs
@@ -16,22 +16,22 @@ defmodule Point do
 end
 
 defmodule Rectangle do
-  def new {:ul, {:point, x1, y1}},
-          {:ur, {:point, x2, y2}},
-          {:ll, {:point, x3, y3}},
-          {:lr, {:point, x4, y4}} do
-    {:rectangle, {:ul, {:point, x1, y1}}, 
-                 {:ur, {:point, x2, y2}},
-                 {:ll, {:point, x3, y3}},
-                 {:rr, {:point, x4, y4}}}
+  def new {:point, x1, y1},
+          {:point, x2, y2},
+          {:point, x3, y3},
+          {:point, x4, y4} do
+    {:rectangle, {:point, x1, y1},
+                 {:point, x2, y2},
+                 {:point, x3, y3},
+                 {:point, x4, y4}}
   end
 
-  def area {:rectangle, {:ul, ul}, {:ur, ur}, _ll, {:lr, lr}} do
+  def area {:rectangle, ul, ur, _ll, lr} do
     Point.distance(ul, ur) * Point.distance(ur, lr)
   end
 
-  def perimeter {:rectangle, {:ul, ul_pt}, {:ur, ur_pt}, _ll, {:lr, lr_pt}} do
-    (Point.distance(ul_pt, ur_pt) + Point.distance(ur_pt, lr_pt)) * 2.0
+  def perimeter {:rectangle, ul, ur, _ll, lr} do
+    (Point.distance(ul, ur) + Point.distance(ur, lr)) * 2.0
   end
 end
 
@@ -39,6 +39,6 @@ ul = Point.new(1,10)
 ur = Point.new(9,10)
 ll = Point.new(1,4)
 lr = Point.new(9,4)
-r1 = Rectangle.new({:ul, ul}, {:ur, ur}, {:ll, ll}, {:lr, lr})
-#Rectangle.area(r1)
-#Rectangle.perimeter(r1)
+r1 = Rectangle.new(ul, ur, ll, lr)
+Rectangle.area(r1)
+Rectangle.perimeter(r1)
