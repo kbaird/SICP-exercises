3aef24bfa41c7b96f187f481b35c99c046fd5a4a
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-13 17:03:25 -0400

    Add use of functions in ex2_02.exs

--- Ch02/ex2_02.exs
+++ Ch02/ex2_02.exs
@@ -8,19 +8,30 @@ end
 
 defmodule Segment do
   def new {:point, x1, y1}, {:point, x2, y2} do
-    {:segment, {:point, x1, y1}, {:point, x2, y2}}
+    {:segment, Point.new(x1, y1), Point.new(x2, y2)}
   end
   def start_pt {:segment, {:point, x1, y1}, _} do
-    {:point, x1, y1}
+    Point.new(x1, y1)
   end
   def end_pt {:segment, _, {:point, x2, y2}} do
-    {:point, x2, y2}
+    Point.new(x2, y2)
   end
   def mid_pt {:segment, {:point, x1, y1}, {:point, x2, y2}} do
-    {:point, average(x1,x2), average(y1,y2)}
+    Point.new(average(x1,x2), average(y1,y2))
   end
 
   # Private functions
   defp average x, y do (x + y) / 2 end
 end
 
+pt1 = Point.new(0,0)
+pt2 = Point.new(3,4)
+Point.x(pt1)
+Point.y(pt1)
+Point.x(pt2)
+Point.y(pt2)
+
+s1 = Segment.new(pt1, pt2)
+Segment.start_pt(s1)
+Segment.end_pt(s1)
+Segment.mid_pt(s1)
