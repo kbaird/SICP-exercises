c76d27e78c04ef288e32f417485c6a5bef930a5a
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-13 19:20:30 -0400

    Add ex2_05.exs

---
+++ Ch02/ex2_05.exs
@@ -0,0 +1,26 @@
+# SICP in Erlang: Exercise 2.5 - cons, car, cdr via 2 ** a * 3 ** b
+
+defmodule Ex2_05 do
+  def cons a, b do
+    trunc(:math.pow(2, a) * :math.pow(3, b))
+  end
+  def car z do
+    extract(z, 0, 2)
+  end
+  def cdr z do
+    extract(z, 0, 3)
+  end
+
+  # Private functions
+  defp extract(encoded, candidate, base) do
+    case rem(encoded, base) do
+      0 -> extract(div(encoded, base), (candidate+1), base)
+      _ -> candidate
+    end
+  end
+end
+
+list = Ex2_05.cons(1,2)
+IO.puts Ex2_05.car(list)
+IO.puts Ex2_05.cdr(list)
+
