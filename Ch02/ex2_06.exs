c9636a57ba6d199142b743d924b4bc03d571f335
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-13 19:23:33 -0400

    Add ex2_06.exs

---
+++ Ch02/ex2_06.exs
@@ -0,0 +1,15 @@
+# SICP in Erlang: Exercise 2.6 - Church Numerals
+
+defmodule Ex2_06 do
+  def church_encode n do
+    fn(f) -> (fn(x) -> inner_encode(f, n, x) end) end
+  end
+
+  # Private functions
+  defp inner_encode(_, 0, x) do x end
+  defp inner_encode(f, n, x) do
+    f.(inner_encode(f, n-1, x))
+  end
+end
+
+Ex2_06.church_encode(15)
