35230b9626b379d45769fdc0bef448805205d513
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-13 19:14:14 -0400

    Add ex2_04.exs

---
+++ Ch02/ex2_04.exs
@@ -0,0 +1,17 @@
+# SICP in Erlang: Exercise 2.4 - cons, car, cdr
+
+defmodule Ex2_04 do
+  def cons x, y do
+    fn(m) -> m.(x, y) end
+  end
+  def car z do
+    z.(fn(p, _) -> p end)
+  end
+  def cdr z do
+    z.(fn(_, q) -> q end)
+  end
+end
+
+list = Ex2_04.cons(1,2)
+IO.puts Ex2_04.car(list)
+IO.puts Ex2_04.cdr(list)
