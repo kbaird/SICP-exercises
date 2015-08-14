1eb3c27568616220338a83987ba03e5db93fc25d
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-14 14:56:56 -0400

    Use point and segment records

--- Ch02/ex2_02.erl
+++ Ch02/ex2_02.erl
@@ -2,18 +2,27 @@
 -author("Kevin C. Baird").
 -purpose("SICP in Erlang: Exercise 2.2 - Represent line segments on a plane.").
 -export([make_point/2, make_segment/2, x_point/1, y_point/1, start_segment/1, end_segment/1, midpoint_segment/1]).
+-record(point,   {x,y}).
+-record(segment, {start_pt,end_pt}).
 
 % creators
-make_point(X, Y) -> {point, X, Y}.
+make_point(X, Y) -> #point{x=X,y=Y}.
 make_segment({point, X1, Y1}, {point, X2, Y2}) ->
-   {segment, {point, X1, Y1}, {point, X2, Y2}}.
+  #segment{start_pt=make_point(X1,Y1), end_pt=make_point(X2, Y2)}.
 
 % extractors
-x_point({point, X, _}) -> X.
-y_point({point, _, Y}) -> Y.
-start_segment(   {segment, {point, X1, Y1}, {point, _X, _Y}}) -> {point, X1, Y1}.
-end_segment(     {segment, {point, _X, _Y}, {point, X2, Y2}}) -> {point, X2, Y2}.
-midpoint_segment({segment, {point, X1, Y1}, {point, X2, Y2}}) -> {point, average(X1,X2), average(Y1,Y2)}.
+x_point(P)          -> P#point.x.
+y_point(P)          -> P#point.y.
+start_segment(S)    -> S#segment.start_pt.
+end_segment(S)      -> S#segment.end_pt.
+midpoint_segment(S) ->
+  P1 = S#segment.start_pt,
+  P2 = S#segment.end_pt,
+  X1 = P1#point.x,
+  Y1 = P1#point.y,
+  X2 = P2#point.x,
+  Y2 = P2#point.y,
+  make_point(average(X1,X2), average(Y1,Y2)).
 
 % hidden helpers
 average(X1, X2) -> (X1+X2) / 2.
