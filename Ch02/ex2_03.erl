67cd35f6be307ecb535acfb295dac19f6c74fec6
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-14 15:03:51 -0400

    Use records in ex2_03.erl

--- Ch02/ex2_03.erl
+++ Ch02/ex2_03.erl
@@ -2,24 +2,27 @@
 -author("Kevin C. Baird").
 -purpose("SICP in Erlang: Exercise 2.3 - Represent rectangle on a plane.").
 -export([make_rectangle/4, perimeter/1, area/1]).
+-import(ex2_03, [make_point/2, x_point/1, y_point/1]).
+-record(point,     {x,y}).
+-record(rectangle, {ul,ur,ll,lr}).
 
 % creators
 make_rectangle({ul, X1, Y1}, {ur, X2, Y2}, {ll, X3, Y3}, {lr, X4, Y4}) ->
-   {rectangle, {ul, X1, Y1}, {ur, X2, Y2}, {ll, X3, Y3}, {lr, X4, Y4}}.
+  #rectangle{ul=make_point(X1,Y1),ur=make_point(X2,Y2),ll=make_point(X3,Y3),lr=make_point(X4,Y4)}.
 
 % extractors
-area({rectangle, {ul, Xul, Yul}, {ur, Xur, Yur}, _LlPoint, {lr, Xlr, Ylr}}) ->
-  (distance({point, Xul, Yul}, {point, Xur, Yur}) *
-   distance({point, Xur, Yur}, {point, Xlr, Ylr})).
+area(R) ->
+  (distance(R#rectangle.ul, R#rectangle.ur) *
+   distance(R#rectangle.ur, R#rectangle.lr)).
 
-perimeter({rectangle, {ul, Xul, Yul}, {ur, Xur, Yur}, _LlPoint, {lr, Xlr, Ylr}}) ->
-  (distance({point, Xul, Yul}, {point, Xur, Yur}) +
-   distance({point, Xur, Yur}, {point, Xlr, Ylr})) * 2.0.
+perimeter(R) ->
+  (distance(R#rectangle.ul, R#rectangle.ur) +
+   distance(R#rectangle.ur, R#rectangle.lr)) * 2.0.
 
 % hidden helpers
-distance(Pt1, Pt2) ->
-  math:sqrt(square(x_diff(Pt1, Pt2)) + square(y_diff(Pt1, Pt2))).
+distance(P1, P2) ->
+  math:sqrt(square(x_diff(P1, P2)) + square(y_diff(P1, P2))).
 square(X) -> X * X.
-x_diff({point, X1, _ }, {point, X2, _ }) -> X2 - X1.
-y_diff({point, _ , Y1}, {point, _ , Y2}) -> Y2 - Y1.
+x_diff(P1, P2) -> P2#point.x - P1#point.x.
+y_diff(P1, P2) -> P2#point.y - P1#point.y.
 
