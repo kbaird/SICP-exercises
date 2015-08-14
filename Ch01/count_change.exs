47dce793aca590f7b5a322384f12991b0a12acd2
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-14 11:16:54 -0400

    More idiomatic module/function naming

--- Ch01/count_change.exs
+++ Ch01/count_change.exs
@@ -1,25 +1,25 @@
 # SICP in Elixir: Count the number of ways to make change
 
-defmodule CountChange do
-  def count_change amount do
+defmodule Change do
+  def count amount do
     # Assume US coinage (five types of coins), and call private function.
-    count_change(amount, 5)
+    count(amount, 5)
   end
 
 # Private functions
-  defp count_change 0, _ do
+  defp count 0, _ do
     1 # There is only one (empty) way to make change for no money at all.
   end
-  defp count_change _, 0 do
+  defp count _, 0 do
     0 # There is no way to make change when there are no coin types available.
   end
-  defp count_change(amount, _) when (amount < 0) do
+  defp count(amount, _) when (amount < 0) do
     0 # There is no way to make change for a negative amount of money.
   end
   # in all other cases...
-  defp count_change(amount, kinds_of_coins) do
-    count_change(amount, kinds_of_coins - 1) +
-      count_change(amount - first_denomination(kinds_of_coins), kinds_of_coins)
+  defp count(amount, kinds_of_coins) do
+    count(amount, kinds_of_coins - 1) +
+      count(amount - first_denomination(kinds_of_coins), kinds_of_coins)
   end
   defp first_denomination(1) do 1  end
   defp first_denomination(2) do 5  end
@@ -28,4 +28,4 @@ defmodule CountChange do
   defp first_denomination(5) do 50 end
 end
 
-IO.puts CountChange.count_change(100)
+IO.puts Change.count(100)
