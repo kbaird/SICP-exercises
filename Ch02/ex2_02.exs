e2d8f6baa8f6f40f79ccb3aa246776408d8329d2
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-16 14:51:01 -0400

    Convert Point to a Struct

--- Ch02/ex2_02.exs
+++ Ch02/ex2_02.exs
@@ -1,22 +1,21 @@
 # SICP in Elixir: Exercise 2.2 - Represent line segments on a plane
 
 defmodule Point do
-  def new(x, y), do: {:point, x, y}
-  def x({:point, x, _}), do: x
-  def y({:point, _, y}), do: y
+  defstruct x: nil, y: nil
+  def new(x, y), do: %Point{x: x, y: y}
 end
 
 defmodule Segment do
-  def new {:point, x1, y1}, {:point, x2, y2} do
+  def new %Point{x: x1, y: y1}, %Point{x: x2, y: y2} do
     {:segment, Point.new(x1, y1), Point.new(x2, y2)}
   end
-  def start_pt {:segment, {:point, x1, y1}, _} do
+  def start_pt {:segment, %Point{x: x1, y: y1}, _} do
     Point.new(x1, y1)
   end
-  def end_pt {:segment, _, {:point, x2, y2}} do
-    Point.new(x2, y2)
+  def end_pt {:segment, _, %Point{x: x1, y: y1}} do
+    Point.new(x1, y1)
   end
-  def mid_pt {:segment, {:point, x1, y1}, {:point, x2, y2}} do
+  def mid_pt {:segment, %Point{x: x1, y: y1}, %Point{x: x2, y: y2}} do
     Point.new(average(x1,x2), average(y1,y2))
   end
 
@@ -26,10 +25,10 @@ end
 
 pt1 = Point.new(0,0)
 pt2 = Point.new(3,4)
-Point.x(pt1)
-Point.y(pt1)
-Point.x(pt2)
-Point.y(pt2)
+pt1.x
+pt1.y
+pt2.x
+pt2.y
 
 s1 = Segment.new(pt1, pt2)
 Segment.start_pt(s1)
