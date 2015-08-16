4428c2c296f6b4fe106137bdf765508f3d000dab
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-16 17:26:20 -0400

    Add ex2_26.exs

---
+++ Ch02/ex2_26.exs
@@ -0,0 +1,20 @@
+# SICP in Elixir: Exercise 2.26 - playing with append/cons/list
+
+defmodule Ex2_26 do
+  def append(x, y), do: x ++ y
+  def cons(x, y),   do: [ x | y ]
+  def list(x, y),   do: [ x, y ]
+end
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+
+result = Ex2_26.append(x, y)
+unless result == [ 1, 2, 3, 4, 5, 6 ], do: throw result
+
+result = Ex2_26.cons(x, y)
+unless result == [ [ 1, 2, 3 ], 4, 5, 6 ], do: throw result
+
+result = Ex2_26.list(x, y)
+unless result == [ [ 1, 2, 3 ], [ 4, 5, 6 ] ], do: throw result
+
