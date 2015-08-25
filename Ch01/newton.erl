2e55fb47b2f86ae02a735347ffcf76e87d0771fa
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-25 12:32:20 -0700

    Add EUnit tests

--- Ch01/newton.erl
+++ Ch01/newton.erl
@@ -2,6 +2,7 @@
 -author("Kevin C. Baird").
 -purpose("SICP in Erlang: Exercise 1.1.7 - Calculate square roots using Newton's method.").
 -export([sqrt/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 sqrt(X) -> sqrt(1.0, X).
 
@@ -20,3 +21,7 @@ average(X, Y) -> (X + Y) / 2.0.
 square(X)     -> X * X.
 tolerance()   -> 0.001.
 
+%% TESTS
+
+not_too_high_test() -> ?assert(newton:sqrt(4) < 2.00001).
+not_too_low_test()  -> ?assert(newton:sqrt(4) > 1.99999).
