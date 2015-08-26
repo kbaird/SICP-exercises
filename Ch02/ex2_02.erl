31761d96efbf13e02ef82442a180896fab4bffd0
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-26 08:37:09 -0700

    Even more formatting

--- Ch02/ex2_02.erl
+++ Ch02/ex2_02.erl
@@ -10,15 +10,21 @@
 
 % creators
 make_point(X, Y) -> #point{x=X,y=Y}.
-make_segment(#point{x=X1, y=Y1}, #point{x=X2, y=Y2}) ->
-  #segment{start_pt=make_point(X1,Y1), end_pt=make_point(X2, Y2)}.
+
+make_segment(#point{x=X1, y=Y1},
+             #point{x=X2, y=Y2}) ->
+  #segment{start_pt=make_point(X1,Y1),
+           end_pt=make_point(X2, Y2)}.
 
 % extractors
 x_point(#point{x=X}) -> X.
 y_point(#point{y=Y}) -> Y.
+
 start_segment(#segment{start_pt=P}) -> P.
 end_segment(#segment{end_pt=P})     -> P.
-midpoint_segment(#segment{start_pt=#point{x=X1,y=Y1},end_pt=#point{x=X2,y=Y2}}) ->
+
+midpoint_segment(#segment{start_pt=#point{x=X1,y=Y1},
+                          end_pt=#point{x=X2,y=Y2}}) ->
   make_point(average(X1,X2), average(Y1,Y2)).
 
 % hidden helpers
