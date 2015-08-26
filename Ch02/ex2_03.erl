bc44cb5f4564296879f699105b39097c9878bcca
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-26 08:36:15 -0700

    More formatting

--- Ch02/ex2_03.erl
+++ Ch02/ex2_03.erl
@@ -7,7 +7,10 @@
 
 % creators
 make_rectangle({ul, X1, Y1}, {ur, X2, Y2}, {ll, X3, Y3}, {lr, X4, Y4}) ->
-  #rectangle{ul=make_point(X1,Y1),ur=make_point(X2,Y2),ll=make_point(X3,Y3),lr=make_point(X4,Y4)}.
+  #rectangle{ul=make_point(X1,Y1),
+             ur=make_point(X2,Y2),
+             ll=make_point(X3,Y3),
+             lr=make_point(X4,Y4)}.
 
 % extractors
 area(#rectangle{ul=UL,ur=UR,lr=LR}) ->
@@ -19,7 +22,10 @@ perimeter(#rectangle{ul=UL,ur=UR,lr=LR}) ->
 % hidden helpers
 distance(P1, P2) ->
   math:sqrt(square(x_diff(P1, P2)) + square(y_diff(P1, P2))).
+
 square(X) -> X * X.
+
 x_diff(#point{x=X1}, #point{x=X2}) -> X2 - X1.
+
 y_diff(#point{y=Y1}, #point{y=Y2}) -> Y2 - Y1.
 
