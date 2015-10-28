c9dd0ec941e9a9bfaca640d147c4c32d340f1054
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-10-28 15:11:17 -0700

    Add ex2_20.erl

---
+++ Ch02/ex2_20.erl
@@ -0,0 +1,15 @@
+-module(ex2_20).
+-author("Kevin C. Baird").
+-purpose("SICP in Erlang: Exercise 2.20 - same_parity.").
+-export([same_parity/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+same_parity([Hd|Tl]) ->
+    SameParityPred = fun(X) -> (X rem 2) =:= (Hd rem 2) end,
+    lists:all(SameParityPred, Tl).
+
+%%% TESTS
+
+all_even_test() -> ?assert(same_parity([0,2,4,6,8]) =:= true).
+all_odd_test()  -> ?assert(same_parity([1,3,5,7,9]) =:= true).
+mixed_test()    -> ?assert(same_parity([0,1])       =:= false).
