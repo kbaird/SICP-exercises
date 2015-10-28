c41bb56e8e0bfabac42fe59367c28de6bc869910
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-10-28 12:11:27 -0700

    Add ex2_17.erl

---
+++ Ch02/ex2_17.erl
@@ -0,0 +1,13 @@
+-module(ex2_17).
+-author("Kevin C. Baird").
+-purpose("SICP in Erlang: Exercise 2.17 - last_pair.").
+-export([last_pair/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+last_pair([X, Y])     -> [X, Y];
+last_pair([_,X|Tail]) -> last_pair([X|Tail]).
+
+%%% TESTS
+
+pair_from_pair_test()  -> ?assert(last_pair([1,2])   =:= [1,2]).
+pair_from_three_test() -> ?assert(last_pair([0,1,2]) =:= [1,2]).
