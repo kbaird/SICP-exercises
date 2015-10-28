6d106d34036f346fafe9b3cd0f7cbb7a60e5f8d7
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-10-28 15:29:06 -0700

    Add ex2_22.erl
    I think a list comprehension is the most idiomatic option in Erlang, however.

---
+++ Ch02/ex2_22.erl
@@ -0,0 +1,18 @@
+-module(ex2_22).
+-author("Kevin C. Baird").
+-purpose("SICP in Erlang: Exercise 2.22 - make square_list iterative.").
+-export([square_list/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+square_list(L) -> lists:reverse(square_list(L, [])).
+
+% PRIVATE HELPER FUNCTIONS
+
+square_list([], Answer)    -> Answer;
+quare_list([H|T], Answer) -> square_list(T, [square(H)|Answer]).
+
+square(X) -> X * X.
+
+%%% TESTS
+
+sq_test() -> ?assert(square_list([0,1,2,3,4]) =:= [0,1,4,9,16]).
