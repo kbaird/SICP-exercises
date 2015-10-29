f6bb4ba1a18bf648b1d1e8a26c18ed006a2168c3
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-10-29 14:22:14 -0700

    Replace raw numeric list with lists:seq/2

--- Ch02/ex2_18.erl
+++ Ch02/ex2_18.erl
@@ -10,6 +10,6 @@ reverse([Hd|Tl]) -> reverse(Tl) ++ [Hd].
 %%% TESTS
 
 reverse_test() ->
-    ?assert(reverse([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) =:=
+    ?assert(reverse(lists:seq(0, 9)) =:=
                     [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]).
 
