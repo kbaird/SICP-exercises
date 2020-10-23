75c6f8abf01f98a723c61f820ee9197fbe0d9871
Kevin Baird <kevincbaird@gmail.com>
2020-10-23 13:27:24 -0700

    Move "private" functions into Newton.Internal.hs, as per https://stackoverflow.com/a/14379426

--- Ch01/Newton.hs
+++ Ch01/Newton.hs
@@ -10,23 +10,8 @@
 
 module Newton where
 
+import Newton.Internal
+
 sqrt :: Float -> Float
 sqrt = sqrtIter 1.0
 
-improve :: Float -> Float -> Float
-improve guess x = (guess + (x/guess)) / 2.0
-
-goodEnough :: Float -> Float -> Bool
-goodEnough guess x =
-  (abs ((square guess) - x)) < tolerance
-
-square :: Float -> Float
-square x = x * x
-
-sqrtIter :: Float -> Float -> Float
-sqrtIter guess x
-  | goodEnough guess x = guess
-  | otherwise          = sqrtIter (improve guess x) x
-
-tolerance :: Float
-tolerance = 0.001
