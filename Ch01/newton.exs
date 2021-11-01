5b7d5b2d93128fc3c6183cb4cbeda2024d5d97e6
Kevin Baird <kevincbaird@gmail.com>
2021-10-31 20:23:16 -0700

    @tolerance and good_enough?/1

--- Ch01/newton.exs
+++ Ch01/newton.exs
@@ -1,18 +1,22 @@
 # SICP in Erlang: Exercise 1.1.7 - Calculate square roots using Newton's method.
 
 defmodule Newton do
+
+  @tolerance 0.001
+
   def sqrt(x), do: sqrt(1.0, x)
 
   # Private functions
   defp sqrt(guess, x) do
-    case good_enough(guess, x) do
-      true -> guess
-      false -> sqrt(improve(guess, x), x)
+    if good_enough?(guess, x) do
+      guess
+    else
+      sqrt(improve(guess, x), x)
     end
   end
 
-  defp good_enough(guess, x) do
-    abs(square(guess) - x) < tolerance
+  defp good_enough?(guess, x) do
+    abs(square(guess) - x) < @tolerance
   end
 
   defp improve(guess, x) do
@@ -21,7 +25,6 @@ defmodule Newton do
 
   defp average(x, y), do: (x + y) / 2.0
   defp square(x), do: x * x
-  defp tolerance, do: 0.001
 end
 
 IO.puts(Newton.sqrt(2))
